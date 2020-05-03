92bd56fcde139a2f744c351ac86ffe970aa72b61
Jochen Grün <jochen.gruen@googlemail.com>
2020-05-03 16:03:52 +0200

    added one more test

--- app/server_test.go
+++ app/server_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"io/ioutil"
 	"fmt"
 	"net/http"
@@ -70,4 +72,51 @@ func TestServerReturnsEmptyListOfLogSources(t *testing.T) {
 	assert.Nil(t, err)
 
 	assert.Equal(t, "[]", string(bodyBytes))
+}
+
+
+
+func TestServerReturnsListOfLogSourcesAfterCreatingOne(t *testing.T) {
+	server := createTestServer(t)
+
+	testFolder := ensureTestingFolder(t)
+
+	defer func() {
+		server.Shutdown()
+		testFolder.EnsureDeleted(t)
+	}()
+
+	err := server.RunAsync()
+	assert.Nil(t, err)
+
+	jobSource := JobLogSource{
+		Name: "jls",
+		SourceFolder: "/mnt",
+		FileRegex: "*.log",
+		SuccessRegex: "SUCCESS",
+		ErrorRegex: "ERROR",
+	}
+	jlsString, err := json.Marshal(jobSource)
+	assert.Nil(t, err)
+
+	respPost, err := http.Post(fmt.Sprintf("http://%s:%s/api/jobsource", server.Address, server.Port), "application/json", bytes.NewBuffer(jlsString))
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusOK, respPost.StatusCode)
+
+	resp, err := http.Get(fmt.Sprintf("http://%s:%s/api/sources", server.Address, server.Port))
+	assert.Nil(t, err)
+	defer resp.Body.Close()
+
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	assert.Nil(t, err)
+	// t.Logf("Got response %s", string(bodyBytes))
+
+	jobSources := make([]LogSource, 0)
+	err = json.Unmarshal(bodyBytes, &jobSources)
+	assert.Nil(t, err)
+
+	assert.Equal(t, 1, len(jobSources))
+	assert.Equal(t, jobSource.Name, jobSources[0].Name)
 }
